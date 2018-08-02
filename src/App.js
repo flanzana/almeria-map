477093acf7fdadafea72345e7f22515df5d3d3d1
Zana Flander <zana.flander@gmail.com>
2018-08-02 11:38:27 +0200

    added menu

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js'
+import Menu from './components/Menu.js'
 
 class App extends Component {
+  state = {
+    menuShow: true
+  }
+
+  /* Hide or show menu */
+  toggleMenu = () => {
+    const menuShow = this.state.menuShow;
+    this.setState({menuShow : !menuShow})
+  }
+
   render() {
+    const { menuShow } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
+          <button id="hamburger-icon" onClick={this.toggleMenu}>
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </button>
           <h1 className="App-title">Almeria</h1>
         </header>
 
-        <i className="fa fa-bars" id="hamburger-icon" aria-hidden="true"></i>
-
         <div className="App-main">
-          {/*<div id="menu-container">aa menu aaaa</div>*/}
-          <Map/>
+
+          {menuShow === true ? <Menu/> : <div className="App-menu-hidden"></div>}
+
+          <section className="App-map"><Map/></section>
         </div>
 
         <footer className="App-footer">
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
+
 class Map extends Component {
    render() {
-   const GoogleMapExample = withGoogleMap(props => (
+   const AlmeriaMap = withGoogleMap(props => (
       <GoogleMap
         defaultCenter = { { lat: 36.838647, lng: -2.46002 } }
         defaultZoom = { 14 }
       >
       </GoogleMap>
    ));
+   
    return(
       <div id="map-container">
-        <GoogleMapExample
+        <AlmeriaMap
           containerElement={ <div className="container-element"/> }
           mapElement={ <div className="map-element"/> }
         />
