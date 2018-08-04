3c6fc14f2ffcfa62096c2dccefd38c8c4676d9d5
Zana Flander <zana.flander@gmail.com>
2018-08-04 22:42:43 +0200

    content of info window added

--- src/components/InfoWinContent.js
+++ src/components/InfoWinContent.js
@@ -9,7 +9,17 @@ class InfoWinContent extends Component {
 	console.log(this.props);
 
 		return(
-			<div>{title}</div>
+			<div className="info-window">
+				<h3 className="info-title">{title}</h3>
+				<p className="info-category">{category}</p>
+				
+				{/* If address is undefined, disply text as no address found.*/}
+				{address &&
+				<p className="info-address">{address}, Almería, Spain</p>}
+				{address === undefined &&
+				<p className="info-address">No address found on Foursquare.</p>}
+				
+			</div>
 		);
 	}
 };
