ee0a69b99eda5739d4892a56e99cd08d139c412b
Zana Flander <zana.flander@gmail.com>
2018-08-07 09:39:58 +0200

    animation added for selected marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,3 +1,5 @@
+/*global google*/
+
 import React, { Component } from 'react';
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
@@ -35,7 +37,9 @@ const AlmeriaMap = compose(
 						title={marker.name}
 						position={marker.location}
 						onClick={onClick}
-						//animation={}
+						animation={//animation only for selected marker: https://stackoverflow.com/questions/44729776/how-can-animation-be-added-to-markers-in-react-google-maps
+							(props.selectedMarker === marker) && (google.maps.Animation.BOUNCE)
+						}
 					>
 						{/*help: https://gist.github.com/jwo/43b382fc60eb09d3a415c9953f4057f8*/}
 						{props.selectedMarker === marker && <InfoWindow onClick>
@@ -54,6 +58,7 @@ const AlmeriaMap = compose(
 	)
 });
 /* ------------ END OF COMPOSE A MAP WITH REACT-GOOGLE-MAPS ------------ */
+
 
 
 class Map extends Component {
